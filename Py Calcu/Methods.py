7b1e75d621eaf90f340fa2434a67f026c4f2a5bf
Resentful <raneljohnt@gmail.com>
2021-06-30 21:25:32 +0800

    Methods for calcu

---
+++ Py Calcu/Methods.py
@@ -0,0 +1,19 @@
+class Methods:
+
+    def Add(n1,n2):
+        return n1+n2
+    
+    def Sub(n1,n2):
+        if n1 >= n2:
+            return n1-n2
+        else:
+            print("Invalid input. Please Try again!")
+
+    def Multi(n1,n2):
+        return n1*n2
+    
+    def Division(n1,n2):
+        if n1 > n2:
+            return n1 / n2
+        else:
+            print("Invalid Input. Please Try again!")
